c4af3f3fb462d783f739f6ee7bb6e159fdafc56c
JohanK <klausenjohan23@gmail.com>
2021-09-06 19:08:08 +0200

    counting words

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -6,7 +6,27 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Skriv en mening");
+            string mening = Console.ReadLine();
+
+            string[] orden = new string [99999];
+            int ordmangd = 0;
+
+            for (int i = 0; i < mening.Length; i++)
+            {
+                if (mening[i] == ' ')
+                {
+                    ordmangd++;
+                }
+                else
+                {
+                    orden[ordmangd] += mening[i];
+                }
+            }
+            ordmangd++;
+            Console.WriteLine(ordmangd);
+
+
         }
     }
 }
