9908ae52b71293522efc5ef94d1ef9c7b96649cc
JohanK <klausenjohan23@gmail.com>
2021-09-08 15:22:00 +0200

    funktionen fortsätter tills man skriver 0

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Ex6
 {
+    //Be användaren gissa ett tal mellan 1-21
+    //Skapa ett slumptal mellan 1-21 (Se Object Browser Random, Random.Next(1,21) och MSDN)
+    //Om användaren gissat rätt: skriv ut Grattis!
+    //Annars fortsätt be användaren gissa
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Gissa på ett tal mellan 1-21");
+                string siffror = Console.ReadLine();
+
+                if (siffror == "0")
+                {
+                    break;
+                }
+
+
+            }
+            Console.WriteLine("Grattis!");
+
         }
     }
 }
