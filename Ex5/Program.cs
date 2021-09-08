e93fc202726afe7fff143d39d1105ad1a4e2b4ab
JohanK <klausenjohan23@gmail.com>
2021-09-08 11:18:19 +0200

    färdig

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -4,9 +4,30 @@ namespace Ex5
 {
     class Program
     {
+        //Låt användaren mata in ett ord i taget
+        //Avsluta programmet om användaren matar in AVSLUTA
+        //Skriv ut din mening just nu är: skriv ut alla inmatade ord
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string mening = "";
+           
+            while (true)
+            {
+
+                Console.WriteLine("Mata in ett ord");
+                string orden = Console.ReadLine();
+                if (orden == "avsluta")
+                {
+
+                    break;
+                }
+                mening = mening + orden + " ";
+
+            } 
+            Console.WriteLine("Alla inmatade orden är:" + mening);
+
+
         }
     }
 }
