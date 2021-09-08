ea69f22220c8484674e08c8567cccdb2171b2596
JohanK <klausenjohan23@gmail.com>
2021-09-08 15:34:28 +0200

    fått ett random nummer

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -11,22 +11,25 @@ namespace Ex6
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+
+            string[] SiffrorMellan1_21 = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+            int sIndex = rnd.Next(SiffrorMellan1_21.Length);
+            Console.WriteLine(SiffrorMellan1_21[sIndex]);
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Gissa på ett tal mellan 1-21");
+            //    string siffror = Console.ReadLine();
+                
+            //    if (siffror == "0")
+            //    {
+            //        break;
+            //    }
 
 
-
-            while (true)
-            {
-                Console.WriteLine("Gissa på ett tal mellan 1-21");
-                string siffror = Console.ReadLine();
-
-                if (siffror == "0")
-                {
-                    break;
-                }
-
-
-            }
-            Console.WriteLine("Grattis!");
+            //}
+            //Console.WriteLine("Grattis!");
 
         }
     }
